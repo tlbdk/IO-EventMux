37fb10291a330076bbb228e6a27335e3c0c1f335
tlb <tlb@c2514f42-cbde-0310-854e-8ab5f9d4de2d>
2006-12-22 14:43:42 +0000

    Remove todo and add sleep
    git-svn-id: https://svn.rapanden.dk/svn/netlookup/trunk@1494 c2514f42-cbde-0310-854e-8ab5f9d4de2d

--- socket-send-error-2.6.19.c
+++ socket-send-error-2.6.19.c
@@ -1,3 +1,4 @@
+// gcc -o socket-send-error-2.6.19 socket-send-error-2.6.19.c
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -17,7 +18,7 @@ int main() {
         ssize_t sent;
         struct sockaddr_in addr;
 
-        fd = socket(PF_INET, SOCK_DGRAM, 0); // TODO
+        fd = socket(PF_INET, SOCK_DGRAM, 0);
         if (fd < 0) {
             perror("socket()");
             return 1;
@@ -34,6 +35,6 @@ int main() {
     }
 
     fprintf(stderr, "Now try 'ping 127.0.0.1' in another shell\n");
-    pause();
+    sleep(100);
     return 0;
 }
