462cdf00962d163a7a10a8a07b57aa2ec584fa37
tlb <tlb@c2514f42-cbde-0310-854e-8ab5f9d4de2d>
2006-12-22 14:21:48 +0000

    git-svn-id: https://svn.rapanden.dk/svn/netlookup/trunk@1492 c2514f42-cbde-0310-854e-8ab5f9d4de2d

--- socket-send-error-2.6.19.pl
+++ socket-send-error-2.6.19.pl
@@ -9,24 +9,22 @@ use Socket;
 
 my $s = IO::Select->new();
 
-foreach my $i (1..338) {
+foreach my $i (1..1000) {
     my $fh = IO::Socket::INET->new(
         Proto    => 'udp',
         Type     => SOCK_DGRAM,
-        Blocking => 0,
+        Blocking => 1,
     ) or die "Could not open socket on 127.0.0.1: $!\n";
 
     $s->add($fh);
     my $addr = pack_sockaddr_in(1000, inet_aton("127.0.0.1"));
-    $fh->send("", 0, $addr);
+    my $rv = $fh->send("", 0, $addr);
+    if(!defined $rv) {
+        print "$i\n";
+    }
 }
-
-
-
+#my @arr = $s->can_write();
+#select(undef, undef, undef, 1.0);
+#sleep 5;
 print "done\n";
-#sleep 1000;
-
-while(1) {
-    my @arr = $s->can_write();
-    print(int(@arr), ": can_write\n");
-}
+sleep 1000;
