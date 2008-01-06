3e5ac824d60b7afb3c1881dd8d0456b1ee296599
tlb <tlb@c2514f42-cbde-0310-854e-8ab5f9d4de2d>
2008-01-06 19:39:08 +0000

    Toy with new api
    git-svn-id: https://svn.rapanden.dk/svn/netlookup/trunk@2111 c2514f42-cbde-0310-854e-8ab5f9d4de2d

---
+++ new-api.pl
@@ -0,0 +1,103 @@
+use strict;
+use warnings;
+
+use IO::EventMux;
+
+# Test Listning TCP sockets
+my $listener = IO::Socket::INET->new(
+    Listen    => 5,
+    LocalPort => $PORT,
+    ReuseAddr => 1,
+) or die "Listening on port $PORT: $!";
+
+my $mux = IO::EventMux->new();
+
+$mux->add($listener, Buffered => new IO::Buffered::Split(qr/\n/));
+$mux->add($listener, Buffered => new IO::Buffered::Size(["n", 0]);
+$mux->add($listener, Buffered => new IO::Buffered::FixedSize(100);
+$mux->add($listener, Buffered => new IO::Buffered::Regexp(qr/(.*?)\n/);
+$mux->add($listener, Buffered => new IO::Buffered::Last();
+$mux->add($listener, Buffered => new IO::Buffered::HTTP();
+
+$mux->add($listener, Buffered => new IO::Buffered(Split => qr/\n/));
+$mux->add($listener, Buffered => new IO::Buffered(Size => ["na*", 0]));
+$mux->add($listener, Buffered => new IO::Buffered(FixedSize => 100));
+$mux->add($listener, Buffered => new IO::Buffered(Regexp => qr/(.*?)\n/));
+$mux->add($listener, Buffered => new IO::Buffered(Last => 1));
+$mux->add($listener, Buffered => new IO::Buffered(HTTP => 1));
+
+my $talker = IO::Socket::INET->new(
+    Proto    => 'tcp',
+    PeerAddr => '127.0.0.1',
+    PeerPort => $PORT,
+    Blocking => 1,
+) or die "Connecting to 127.0.0.1:$PORT: $!\n";
+print "talker:$talker\n";
+$mux->add($talker, Buffered => ["Regexp", qr/(.*\n)/]);
+
+my @dataset = ("data 1\n", "data 2\n", "data 3");
+my @dataset_connecter;
+my @dataset_talker;
+
+$mux->send($talker, @dataset);
+push(@dataset_connecter, @dataset);
+
+my $time = time;
+my $timeout = 1;
+my $connecter;
+my %disconnected_fhs;
+while(1) {
+    my $event = $mux->mux($timeout);
+    my $type  = $event->{type};
+    my $fh    = ($event->{fh} or '');
+    my $data  = ($event->{data} or '');
+
+    print "$fh $type: $data\n";
+
+    if($type eq 'ready') {
+    
+    } elsif($type eq 'accepted') {
+        $connecter = $fh;
+
+    } elsif($type eq 'closing') {
+
+    } elsif($type eq 'closed') {
+        $disconnected_fhs{$fh} = 1;
+        if(keys %disconnected_fhs == 2) { last; }
+
+    } elsif($type eq 'read' and $fh eq $connecter) {
+        my $test = shift @dataset_connecter;
+        
+        # Send some data to talker
+        $mux->send($fh, $test);
+        push(@dataset_talker, $test);
+
+        # Remove the line break before we compare.
+        $test =~ s/\n$//;
+        ok($event->{data} eq $test, 
+            "Read from $fh with Split buffering:".
+            "'$test' = '$event->{data}'");
+
+    } elsif($type eq 'read' and $fh eq $talker) {
+        my $test = shift @dataset_talker;
+        ok($event->{data} eq $test, 
+            "We got god read from $fh with Regexp buffering:". 
+            "'$test' = '$event->{data}'");
+
+    } elsif($type eq 'read_last') {
+    
+    } elsif($type eq 'sent') {
+
+    } elsif($type eq 'timeout') {
+        ok(time-$time-$timeout <= 1, 
+            "Timeout difference was not to long: 1 >= ".(time-$time-$timeout));
+        ok(time-$time-$timeout >= 0, 
+            "Timeout difference was not to short: 0 <= ".(time-$time-$timeout));
+        $mux->close($talker);
+    
+    } else {
+        die("Unhandled event $type");
+    }
+
+}
+
